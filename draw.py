bf0b88c83c5317be8beebbd92b66890f980f9309
Mohammed Reda Belfaida <Mohammed-reda.belfaida@grenoble-inp.org>
2024-07-29 00:16:12 +0100

    new commit is done

--- draw.py
+++ draw.py
@@ -1,22 +1,76 @@
 #!/usr/bin/env python3 
 import sys 
 import subprocess
+from random import randint
+from PIL import Image
 from monte_carlo import *
+
+step=30
+offset=10
+
+segments={
+    0:['a','b','c','d','e','g'],
+    1:['b','c'],
+    2:['a','b','g','d','e'],
+    3:['a','b','f','c','d'],
+    4:['b','c','g','f'],
+    5:['a','f','g','c','d'],
+    6:['a','f','g','e','d','c'],
+    7:['a','b','c'],
+    8:['a','b','c','d','e','f','g'],
+    9:['a','b','c','d','f','g']
+}
+
+light_segments={
+    'a':[(-(step+1),k) for k in range(-(step+1),1)],
+    'b':[(k,0) for k in range(-(step+1),0)],   
+    'c':[(k,0) for k in range(1,step+2)],
+    'd':[(step+1,k) for k in range(-(step+1),1)],
+    'e':[(k,-(step+1)) for k in range(1,step+2)],
+    'f':[(0,k) for k in range(-(step+1),1)],
+    'g':[(k,-(step+1)) for k in range(-(step+1),0)],
+}
 
 pink=(255,0,255)
 blue=(0,0,255)
+black=(0,0,0)
 pink_color="255 0 255"
 blue_color="0 0 255"
 blank_color="255 255 255"
+black_color="0 0 0" 
+ppm_files=[]
 
-def get_decimal_part(number):
-    number=str(number)
-    return int(number[2:])
+def get_str_decimal_part(number):
+    return str(number)[2:]
+
+def search_pixel(point,img_size):
+    x,y=point
+    j=int((x+1)*(img_size-1)/2)
+    i=int((y+1)*(img_size-1)/2)
+    return i,j
+
+def draw_comma(pixels_matrix,starting_pos):
+    i,j=starting_pos
+    for k in range(offset):
+        pixels_matrix[i+step-1][j+k]=black 
+        pixels_matrix[i+step][j+k]=black 
+        pixels_matrix[i+step+1][j+k]=black
+    return pixels_matrix
+
+def draw_number(pixels_matrix,number,starting_pos):
+    assert 0<=number<=9 
+    i,j=starting_pos
+    pixels_matrix[i][j]=black
+    if number==0:
+        pixels_matrix[i][j-step]=black
+    for seg in segments[number]:
+        for offset in light_segments[seg]:
+            pixels_matrix[i+offset[0]][j+offset[1]]=black
+    return pixels_matrix
 
 def generate_ppm_file(image_size,points,state_number,value):
-    integer_part=int(value)
-    decimal_part=get_decimal_part(value)
-    file_name=f"img{state_number}_{integer_part}-{decimal_part}.ppm"
+    value=str(value)
+    file_name=f"img{state_number}_"+value[0]+"-"+value[2:]+".ppm"
     target_file=open(file_name,"w")
     magic_number="P3"
     print(magic_number,file=target_file)
@@ -27,20 +81,29 @@ def generate_ppm_file(image_size,points,state_number,value):
     for point in points : 
         i,j=search_pixel(point,image_size)
         pixels_matrix[i][j]=blue if in_circle(point) else pink 
+    index=len(value)//2
+    i=(image_size//2)+step  # positioning i relatively to the center
+    j= i-(step+offset)*(index-1) # positioning j as well
+    current=(i,j)
+    copy=current
+    pixels_matrix=draw_comma(pixels_matrix,copy)
+    copy=(copy[0],copy[1]-offset)
+    pixels_matrix=draw_number(pixels_matrix,int(value[0]),copy)
+    current=(current[0],current[1]+step+offset)
+    for number in value[2:]:
+        pixels_matrix=draw_number(pixels_matrix,int(number),current)
+        current=(current[0],current[1]+step+offset)
     for i in range(image_size):
         for j in range(image_size):
             if pixels_matrix[i][j] == blue:       
                 print(blue_color,file=target_file)
             elif pixels_matrix[i][j]==pink:
                 print(pink_color,file=target_file)
+            elif pixels_matrix[i][j]==black:
+                print(black_color,file=target_file)
             else:
                 print(blank_color,file=target_file)
-
-def search_pixel(point,img_size):
-    x,y=point
-    j=int((x+1)*(img_size-1)/2)
-    i=int((y+1)*(img_size-1)/2)
-    return i,j
+    ppm_files.append(file_name)
 
     
 def draw():
@@ -58,8 +121,8 @@ def draw():
         points,value=estimate(tmp)
         value=round(value,decimal_places_number)
         generate_ppm_file(image_size,points,i,value)
-
-
+    frames = [Image.open(image) for image in ppm_files]
+    frames[0].save('output.gif', save_all=True, append_images=frames[1:], duration=500, loop=0)
 
 if __name__=="__main__":
     draw()
